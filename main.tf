3140ee75e3b10a38b26c373330c224b7d432ff60
unknown <binita.dazzling@gmail.com>
2023-03-24 19:44:39 +0530

    added cred

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "snowflake" {
   role  = "RL_CICD_INFRA"
+  username="BINITARATH"
+  password="Binit@feb2023"
+  account="az60464.australia-east.azure"
+  
 }
 
 resource "snowflake_database" "db" {
