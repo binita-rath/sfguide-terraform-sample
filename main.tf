708f66d998aba5ce50b4cb4696a1bca2e43f1d4c
unknown <binita.dazzling@gmail.com>
2023-03-24 20:37:15 +0530

    bb

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "snowflake" {
   role  = "RL_CICD_INFRA"
-  #username=var.username
-  #password=var.password
-  #account=var.account
+  username="BINITARATH"
+  password="Binit@feb2023"
+  account="az60464.australia-east.azure"
   
 }
 
 resource "snowflake_database" "db" {
-  name     = "TF1_DEMO_24MAR"
+  name     = "TF1_DEMO_21MAR"
 }
 
