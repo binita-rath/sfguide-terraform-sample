3a325d78bf84675434ffa27f0177d1e49a5dfb40
unknown <binita.dazzling@gmail.com>
2023-03-25 17:27:32 +0530

    added config

--- variables.tf
+++ variables.tf
@@ -1,3 +1,17 @@
-variable "username" {}
-variable "password" {}
-variable "account" {}
+variable "username" {
+  description = "username"
+  type        = string
+  #default     = "BINITARATH"
+}
+
+variable "password" {
+  description = "password"
+  type        = string
+  #default     = "Binit@feb2023"
+}
+
+variable "account" {
+  description = "account"
+  type        = string
+  #default     = "az60464.australia-east.azure"
+}
