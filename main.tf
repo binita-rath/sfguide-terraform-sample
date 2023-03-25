4129ebc7c643b3b624a9ebbe3da48861a383cd13
unknown <binita.dazzling@gmail.com>
2023-03-25 12:30:57 +0530

    added var blocks

--- main.tf
+++ main.tf
@@ -7,9 +7,23 @@ terraform {
   }
 }
 
-variable "username" {}
-variable "password" {}
-variable "account" {}
+variable "username" {
+  description = "username."
+  type        = string
+  #default     = "BINITARATH"
+}
+
+variable "password" {
+  description = "password"
+  type        = string
+  #default     = "Binit@feb2023"
+}
+
+variable "account" {
+  description = "account"
+  type        = string
+  #default     = "az60464.australia-east.azure"
+}
 
 provider "snowflake" {
   role  = "RL_CICD_INFRA"
