f9b7cbaf21001c84b494f12e31ede2de8a96749a
unknown <binita.dazzling@gmail.com>
2023-03-25 17:35:00 +0530

    added config

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "username" {
   description = "username"
   type        = string
-  default     = var.username
+  
 }
 
 variable "password" {
   description = "password"
   type        = string
-  default     = var.password
+  
 }
 
 variable "account" {
   description = "account"
   type        = string
-  default     = var.account
+  
 }
