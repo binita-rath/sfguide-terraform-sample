4ae3baf735dbe36a2c3812631952be4a4702598e
unknown <binita.dazzling@gmail.com>
2023-03-25 20:10:40 +0530

    hmm

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "username" {
+  description = "username"
+  type        = string
+  
+}
+
+variable "password" {
+  description = "password"
+  type        = string
+  
+}
+
+variable "account" {
+  description = "account"
+  type        = string
+  
+}
